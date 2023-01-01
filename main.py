c4c9e5d8ee23b4a52d7efbc4314adc3a90edde45
Chris Asis <cgasis@gmail.com>
2023-01-01 03:07:35 +0000

    added steps for creating SLL

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Creating SLL
+# 1 create head and tail and reference to null
+# 2 create node and assign value to it and reference to null
+# 3 link head and tail to this node
+
 class Node
   def __init__(self, value)
     self.value = value
