b8d2fde0d0851e6c639ad6f46324f63e9f2ead67
Chris Asis <cgasis@gmail.com>
2023-01-01 03:19:06 +0000

    renamed file to CreationSLL.py

--- CreationSLL.py
+++ CreationSLL.py
@@ -1,6 +1,6 @@
 # Creating SLL
 # 1 create head and tail and reference to null
-# 2 create node and assign value to it and reference to null
+# 2 create a blank node and assign value to it and reference to null
 # 3 link head and tail to this node
 
 class Node
